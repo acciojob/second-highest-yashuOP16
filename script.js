590be363dc55634a53075a3e93b6c5ed8adbbfcd
Yash <132873361+yashuOP16@users.noreply.github.com>
2026-03-23 14:53:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length < 2) return -Infinity;
+
+    let first = -Infinity;
+    let second = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+        let num = Number(arr[i]); // prompt se string aata hai
+
+        if (num > first) {
+            second = first;
+            first = num;
+        } else if (num < first && num > second) {
+            second = num;
+        }
+    }
+
+    return second;
+}
